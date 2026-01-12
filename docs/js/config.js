05127b1e29caf189f61853eaab70661112bfa0a6
jiangsir <jiangzero@gmail.com>
2026-01-13 00:35:30 +0800

    CORS

--- docs/js/config.js
+++ docs/js/config.js
@@ -2,7 +2,8 @@
 // 請在部署後將這個 URL 替換為您的 Google Apps Script Web App URL
 const API_CONFIG = {
     // 開發環境 - 請替換為您的 GAS Web App URL
-    API_URL: 'https://script.google.com/macros/s/AKfycbxjxBk04wibqHNR5gBmQu954QPc3fP-FhB-rGo9Bn-amByy3OjIrbakWAkee8vXQqY/exec',
+    // 原本的URL https://script.google.com/macros/s/AKfycbxjxBk04wibqHNR5gBmQu954QPc3fP-FhB-rGo9Bn-amByy3OjIrbakWAkee8vXQqY/exec
+    API_URL: 'https://script.google.com/macros/s/AKfycbwvM3kATp6hrPn7NXb6A8LgRcqtmVpcZKRr4KslYewPnoSxvYGMmY_Oj1dDG9gvftY/exec',
     
     // Google Spreadsheet ID (用於開啟試算表連結)
     SHEET_ID: '12NlutBJAq7HkIO7OE0E5UhpMqE7demarGTlVK5ZD1Sw',
