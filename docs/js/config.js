ae668b04b357425d6f9483cca24da2627ce85ace
jiangsir <jiangzero@gmail.com>
2026-01-13 08:09:57 +0800

    更新 API_URL 至最新的 Google Apps Script Web App URL

--- docs/js/config.js
+++ docs/js/config.js
@@ -4,8 +4,10 @@ const API_CONFIG = {
     // 開發環境 - 請替換為您的 GAS Web App URL
     // 原本的URL https://script.google.com/macros/s/AKfycbxjxBk04wibqHNR5gBmQu954QPc3fP-FhB-rGo9Bn-amByy3OjIrbakWAkee8vXQqY/exec
     // 
-    // https://script.google.com/macros/s/AKfycbwvM3kATp6hrPn7NXb6A8LgRcqtmVpcZKRr4KslYewPnoSxvYGMmY_Oj1dDG9gvftY/exec
-    API_URL: 'https://script.google.com/macros/s/AKfycbzYKrZ3NfWVT8_MpZKx2IAAa5wJCgLOOulv_ojtvNmpLF1DSTTFzO2OcV4OU13ccxA/exec',
+    // https://script.google.com/macros/s/AKfycbwvM3kATp6hrPn7NXb6A8LgRcqtmVpcZKRr4KslYewPnoSxvYGMmY_Oj1dDG9gvftY/exec 
+    // https://script.google.com/macros/s/AKfycbydFSNsZ0pnPHFYECk6egIOKcId1dFNPe_f642aSDACbrBuH1zH4qdE3e5oy6fca74/exec 
+    API_URL: 'https://script.google.com/macros/s/AKfycbydFSNsZ0pnPHFYECk6egIOKcId1dFNPe_f642aSDACbrBuH1zH4qdE3e5oy6fca74/exec', // PadBookings 143版
+    //API_URL: 'https://script.google.com/macros/s/AKfycbzYKrZ3NfWVT8_MpZKx2IAAa5wJCgLOOulv_ojtvNmpLF1DSTTFzO2OcV4OU13ccxA/exec', // PadBookings2 2版
     
     // Google Spreadsheet ID (用於開啟試算表連結)
     SHEET_ID: '12NlutBJAq7HkIO7OE0E5UhpMqE7demarGTlVK5ZD1Sw',
